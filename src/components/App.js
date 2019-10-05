522d27416581ae94595b2946829824381ec0a532
abhishektanwar <abhishektanwar084@gmail.com>
2019-10-05 23:40:23 +0530

    ipfs js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
 
+const ipfsClient = require('ipfs-http-client')
+// connection to an ipfs node
+// const ipfs = ipfsClient('localhost','5001'{protocol:'http'})
+var ipfs = ipfsClient({ host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      buffer:null
+      buffer:null,
+      memeHash:'QmdygKgEmUECQCQhciBeRqEpaNPU81wSxoJbv9VQZGFaEY'
     };
   }
 
@@ -29,10 +34,32 @@ class App extends Component {
     }
 
   }
-
-  onSubmit = (event) => {
+  // hash returned by ipfs:"QmdygKgEmUECQCQhciBeRqEpaNPU81wSxoJbv9VQZGFaEY"
+  onSubmit = async (event) => {
     event.preventDefault()
     console.log('submitting the form.')
+    console.log(this.state.memeHash)
+
+    const results = await ipfs.add(this.state.buffer)
+    console.log('ipfs results',results)
+    const Hash = results[0].hash
+    console.log(Hash)
+    this.setState({memeHash:Hash})
+    console.log(this.state.memeHash)
+
+    
+    // ipfs.add(this.state.buffer,(error,result) => {
+    //   console.log('ipfs results',result)
+
+    //   const memeHash = result.hash
+    //   console.log('memHash1',memeHash)
+    //   this.setState({memeHash:memeHash})
+    //   console.log(this.state.memeHash)
+    //   if(error){
+    //     console.error(error)
+    //     return
+    //   }
+    // })
   }
 
 
@@ -58,11 +85,12 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={'https://ipfs.infura.io/ipfs/'+this.state.memeHash} />
                 </a>
                 <p>&nbsp;</p>
                 <h2>Change meme</h2>
                 <form onSubmit={this.onSubmit}>
+                  
                   <input type='file' onChange={this.captureFile}/>
                   <input type='submit'/>
                   
